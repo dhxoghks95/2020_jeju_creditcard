8a5754f4d114d6b7aaf0710c48aa9c39453971cb
dhxoghks95 <dhxoghks95@gmail.com>
2020-07-14 22:52:47 +0900

    전부 다 머지

--- code/preprocessing_EDA/merge_files.R
+++ code/preprocessing_EDA/merge_files.R
@@ -40,11 +40,27 @@ colnames(tourist) = c("year", "month", "CARD_SIDO_NM", "total_tourist")
 
 temp_merge = temp %>% left_join(tourist, by = c("year", "month", "CARD_SIDO_NM"))
 
-sale_by_indst = read_csv("C:/Users/dhxog/Desktop/데이콘/2020_jeju_creditcard/data/sm_food_cleaned.csv")
+sale_food = read_csv("C:/Users/dhxog/Desktop/데이콘/2020_jeju_creditcard/data/sm_food_cleaned.csv")
+sale_tr = read_csv("C:/Users/dhxog/Desktop/데이콘/2020_jeju_creditcard/data/us_cleaned.csv")
+sale_etc = read_csv("C:/Users/dhxog/Desktop/데이콘/2020_jeju_creditcard/data/gt_cleaned.csv")
 
-sale_by_indst = sale_by_indst %>% select(-X1)
+sale_food = sale_food %>% select(-X1)
+sale_tr = sale_tr %>% select(-X1)
+sale_etc = sale_etc %>% select(-X1)
 
-colnames(sale_by_indst) = c("CARD_SIDO_NM", "STD_CLSS_NM", "total_sale_by_indst")
+colnames(sale_food) = c("CARD_SIDO_NM", "STD_CLSS_NM", "total_sale_by_indst")
+colnames(sale_tr) = c("CARD_SIDO_NM", "STD_CLSS_NM", "total_sale_by_indst")
+colnames(sale_etc) = c("CARD_SIDO_NM", "STD_CLSS_NM", "total_sale_by_indst")
+
+sido = sale_food %>% select(CARD_SIDO_NM) %>% unlist() %>% unique() %>% as.vector()
+
+sale_tr = rep(sido, 4) %>% sort() %>% as.tibble() %>% cbind(sale_tr)
+sale_tr = sale_tr[,-1]
+
+colnames(sale_tr) = c("CARD_SIDO_NM", "STD_CLSS_NM", "total_sale_by_indst")
+sale_tr$total_sale_by_indst = sale_tr$total_sale_by_indst / length(unique(sale_tr$CARD_SIDO_NM))
+
+sale_by_indst = rbind(sale_food, sale_tr, sale_etc)
 
 final_merge = temp_merge %>% left_join(sale_by_indst, by = c("CARD_SIDO_NM", "STD_CLSS_NM"))
 
