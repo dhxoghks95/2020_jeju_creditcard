1b47f0c9e687249fdaf3f5e7bba0cb6b49fc810a
dhxoghks95 <dhxoghks95@gmail.com>
2020-07-17 14:53:54 +0900

    final_merge 수정

--- code/preprocessing_EDA/merge_files.R
+++ code/preprocessing_EDA/merge_files.R
@@ -1,25 +1,26 @@
 library(tidyverse)
 library(lubridate)
-혜연 = read_csv("C:/Users/dhxog/Desktop/데이콘/2020_jeju_creditcard/data/melted_age_sex.csv")
+age_sex = read_csv("C:/Users/dhxog/Desktop/데이콘/2020_jeju_creditcard/data/melted_age_sex(10-70).csv")
+
+
 영화 = read_csv("movie_tidy.csv")
 선우 = read_csv("C:/Users/dhxog/Desktop/데이콘/2020_jeju_creditcard/data/corona_added.csv")
 
 태환 = read.csv("C:/Users/dhxog/Desktop/데이콘/2020_jeju_creditcard/data/add_weather_fest.csv")
 태환 = 태환 %>% mutate(year = as.numeric(year(YYMM)))
 
-colnames(선우)
 
 태환 = 태환 %>% select(-c(X.1, X))
 
-merged %>% select(STD_CLSS_NM) %>% unique()
+
 선우 = 선우 %>% select(-c(X1))
-혜연 = 혜연 %>% select(-c(X1))
+
 
 
 
 merged = merge(선우, 태환, by = c("CARD_SIDO_NM", "STD_CLSS_NM", "HOM_SIDO_NM", "AGE", "SEX_CTGO_CD", "FLC", "CSTMR_CNT", "AMT", "CNT", "month", "year"))
 
-temp = merged %>% left_join(혜연, by = c("CARD_SIDO_NM", "AGE", "SEX_CTGO_CD"))
+temp = merged %>% left_join(age_sex, by = c("CARD_SIDO_NM", "AGE", "SEX_CTGO_CD"))
 
 write.csv(merged, "merged_so_data.csv")
 
@@ -31,12 +32,29 @@ temp = temp %>% left_join(movie, by = c("CARD_SIDO_NM"))
 
 write.csv(temp, "merged_data.csv")
 
+
+
 tourist = read_csv("C:/Users/dhxog/Desktop/데이콘/2020_jeju_creditcard/data/외래관광객.csv")
 tourist = tourist %>% select(-X1)
 tourist_came = read_csv("C:/Users/dhxog/Desktop/데이콘/2020_jeju_creditcard/data/관광객입도현황.csv")
 tourist_came = tourist_came %>% select(-X1)
 
+
 colnames(tourist) = c("year", "month", "CARD_SIDO_NM", "total_tourist")
+
+colnames(tourist_came) = c("year", "month", "CARD_SIDO_NM", "total_tourist")
+
+ratio = tourist_came %>% filter(year == 2019) %>% select(total_tourist) / tourist %>% filter(CARD_SIDO_NM == "제주") %>% select(total_tourist)
+
+ratio_num = ratio %>% unlist() %>% mean()
+
+tourist = tourist %>% mutate(total_tourist = total_tourist * ratio_num)
+
+tourist = tourist %>% filter(CARD_SIDO_NM != "제주")
+
+tourist = rbind(tourist, tourist_came)
+
+
 
 temp_merge = temp %>% left_join(tourist, by = c("year", "month", "CARD_SIDO_NM"))
 
@@ -49,19 +67,26 @@ sale_tr = sale_tr %>% select(-X1)
 sale_etc = sale_etc %>% select(-X1)
 
 colnames(sale_food) = c("CARD_SIDO_NM", "STD_CLSS_NM", "total_sale_by_indst")
-colnames(sale_tr) = c("CARD_SIDO_NM", "STD_CLSS_NM", "total_sale_by_indst")
 colnames(sale_etc) = c("CARD_SIDO_NM", "STD_CLSS_NM", "total_sale_by_indst")
 
 sido = sale_food %>% select(CARD_SIDO_NM) %>% unlist() %>% unique() %>% as.vector()
 
 sale_tr = rep(sido, 4) %>% sort() %>% as.tibble() %>% cbind(sale_tr)
-sale_tr = sale_tr[,-1]
 
 colnames(sale_tr) = c("CARD_SIDO_NM", "STD_CLSS_NM", "total_sale_by_indst")
+
 sale_tr$total_sale_by_indst = sale_tr$total_sale_by_indst / length(unique(sale_tr$CARD_SIDO_NM))
 
 sale_by_indst = rbind(sale_food, sale_tr, sale_etc)
 
+
+
 final_merge = temp_merge %>% left_join(sale_by_indst, by = c("CARD_SIDO_NM", "STD_CLSS_NM"))
+
+
+
+final_merge$total_sale_by_indst = final_merge$total_sale_by_indst %>% as.numeric()
+
+summary(final_merge)
 
 write.csv(final_merge, "final_merge.csv")
