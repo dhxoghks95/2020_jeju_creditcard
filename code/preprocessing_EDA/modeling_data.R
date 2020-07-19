fadb8ea802bad407de1d370a21ab40887e00d1e3
dhxoghks95 <dhxoghks95@gmail.com>
2020-07-19 21:56:07 +0900

    modeling용 데이터 만들기

---
+++ code/preprocessing_EDA/modeling_data.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+
+oh = read.csv("transform_eda.csv")
+
+jo_1 = read_csv("C:/Users/dhxog/Desktop/데이콘/2020_jeju_creditcard/data/indst_data.csv")
+
+jo_2 = read_csv("C:/Users/dhxog/Desktop/데이콘/2020_jeju_creditcard/data/log_indst_data.csv")
+
+kim = read.csv("C:/Users/dhxog/Desktop/데이콘/2020_jeju_creditcard/data/total_tourist_groupby.csv")
+
+oh = oh %>% select(-X)
+jo_1 = jo_1 %>% select(-X1) %>% unique()
+colnames(jo_1) = c("CARD_SIDO_NM", "STD_CLSS_NM", "total_sale_by_indst_groupby")
+jo_2 = jo_2 %>% select(-X1) %>% unique()
+colnames(jo_2) = c("CARD_SIDO_NM", "STD_CLSS_NM", "log_total_sale_by_indst")
+kim = kim %>% select(-X) %>% unique()
+
+data = oh %>% left_join(jo_1, by = c("CARD_SIDO_NM", "STD_CLSS_NM"))
+
+data = data %>% left_join(jo_2, by = c("CARD_SIDO_NM", "STD_CLSS_NM"))
+
+data = data %>% left_join(kim, by = c("total_tourist")) 
+data %>% glimpse()
+
+data = data %>% select(-c(CSTMR_CNT, CNT, YYMM, screen_num, total_tourist, total_sale_by_indst))
+
+write.csv(data, "modeling_data.csv")
+
